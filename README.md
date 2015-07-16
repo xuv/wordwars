e94e17ace6db691e22b48813c089a5ae234c4fd3
Julien Deswaef <juego@requiem4tv.com>
2015-07-16 18:00:10 -0400

    preparing for publication

---
+++ README.md
@@ -0,0 +1,15 @@
+WORD WARS
+=========
+**News from the Empire**
+
+ - Gets the latest news from The New York Times
+ - Filters them by keywords related to WAR
+ - Renders a 720p video mimicking the STAR WARS crawl
+ - Uploads to [Youtube](https://www.youtube.com/channel/UC64gD1NN-GE_tfd0swtxcOA)
+ 
+Project done with Blender, Python, Inkscape, Bash and Gnu/Linux.
+ 
+Copyright [Julien Deswaef](http://xuv.be).
+
+Released under the Free Art License 1.3
+
--- scripts/wordwars.sh
+++ scripts/wordwars.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-DATE=$(date "+%Y%m%d") # As in 20150627 for example
+DATE=$(date "+%Y%m%d-%h") # As in 20150627-01 for example
 # The magic command to get the path where the script works
 SCRIPTPATH=${0%/*}
+
+cd $SCRIPTPATH
 
 BLENDER='blender'
 
@@ -11,32 +13,32 @@ echo $SCRIPTPATH
 
 # Update the Blender file with new content
 
-$BLENDER -b $SCRIPTPATH/../wordwars.blend -P $SCRIPTPATH/fetch.py
+$BLENDER -b ../wordwars.blend -P $SCRIPTPATH/fetch.py
 
 # Render the blender file if there was new content (aka. a description file has been created
 
-DESCRIPTION_FILE=$SCRIPTPATH/../data/description.txt
+DESCRIPTION_FILE=../data/description.txt
 
 if [ -f $DESCRIPTION_FILE ]
 then
     echo FOUND $DESCRIPTION_FILE
     DESCRIPTION=$(<$DESCRIPTION_FILE)
-    $BLENDER -b $SCRIPTPATH/../wordwars.blend -S Prerendered -a
+    $BLENDER -b ../wordwars.blend -S Prerendered -a
 else
 	echo NO description.txt FOUND
 fi
 
 # If there was a render, upload it to Youtube and backup the generated files
 
-RENDER_FILE=$SCRIPTPATH/../render/WordWars0001-2376.avi
+RENDER_FILE=../render/WordWars0001-2376.avi
 TITLE=$(head -n 1 $DESCRIPTION_FILE)
 
 if [ -f $RENDER_FILE ]
 then
     echo FOUND render file
-    #python $SCRIPTPATH/upload.py --file $RENDER_FILE --title $TITLE --description $DESCRIPTION --privacyStatus unlisted
-    mv $DESCRIPTION_FILE $SCRIPTPATH/../data/description-$DATE.txt
-    mv $RENDER_FILE $SCRIPTPATH/../render/WordWars-$DATE.avi
+    python upload.py --file $RENDER_FILE --title "$TITLE" --description "$DESCRIPTION" --privacyStatus unlisted
+    mv $DESCRIPTION_FILE ../data/description-$DATE.txt
+    mv $RENDER_FILE ../render/WordWars-$DATE.avi
 else
 	echo NO render file FOUND
 fi
