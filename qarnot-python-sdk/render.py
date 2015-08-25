231ac838cf19d4beedceec24f51387ac9fd9e206
Julien Deswaef <juego@requiem4tv.com>
2015-08-25 11:30:23 -0400

    playing with qarnot

--- qarnot-python-sdk/render.py
+++ qarnot-python-sdk/render.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
-"""Blender sample"""
+"""Word Wars on radiators"""
 from __future__ import print_function
 import qapy
 import sys
 import os
 import datetime
+import requests.packages.urllib3
 
 def log(line):
     """Log"""
@@ -15,16 +16,19 @@ def main():
     """main"""
     log("Loading config...")
     api = qapy.QApy('qarnot.conf')
+    
+    # Hide SSL security warnings
+    requests.packages.urllib3.disable_warnings()
 
     log("Creating task...")
-    with api.create_task("blender render", "blender", 1) as task:
+    with api.create_task("Word wars test render", "blender", 1) as task:
         for disk in api.disks():
             log("Reusing disk " + disk.description)
             task.resources = disk
             break
 
         log("Sync resources from 'input'")
-        task.resources.sync_directory("input", True)
+        task.resources.sync_directory("wordwars", True)
         task.resources.locked = True
         task.resources.commit()
 
@@ -33,10 +37,12 @@ def main():
         #BLEND_ENGINE (CYCLES|BLENDER_RENDER), BLEND_W, BLEND_H, BLEND_RATIO,
         #BLEND_CYCLES_SAMPLES, BLEND_SCENE, BLEND_SLICING, BLEND_FILE,
         #BLEND_FORMAT
-        task.constants['BLEND_FILE'] = "space_corridor_final.blend"
-        task.constants['BLEND_FORMAT'] = "PNG"
-        task.constants['BLEND_ENGINE'] = "CYCLES"
-        task.constants['BLEND_CYCLES_SAMPLES'] = 50
+        task.constants['BLEND_FILE'] = "wordwars-qarnot.blend"
+        task.constants['BLEND_FORMAT'] = "TGA"
+        task.constants['BLEND_ENGINE'] = "BLENDER_RENDER"
+        #task.constants['BLEND_CYCLES_SAMPLES'] = 50
+        
+        task.advanced_range = "[471-475]"
 
         log("Submitting task...")
         task.submit()
